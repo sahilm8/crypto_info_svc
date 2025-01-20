9993f02d23e541cf0c31fd89093e23881f12d4c3
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-20 18:23:25 +0000

    Optimise CryptoController

--- src/main/java/com/sahil/crypto/info/controller/CryptoController.java
+++ src/main/java/com/sahil/crypto/info/controller/CryptoController.java
@@ -4,46 +4,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sahil.crypto.info.dto.getCrypto.GetCryptoRequest;
+import com.sahil.crypto.info.dto.getCrypto.GetCryptoResponse;
+import com.sahil.crypto.info.dto.getCryptoTs.GetCryptoTsRequest;
+import com.sahil.crypto.info.dto.getCryptoTs.GetCryptoTsResponse;
 import com.sahil.crypto.info.model.Crypto;
 import com.sahil.crypto.info.model.CryptoTs;
 import com.sahil.crypto.info.service.CryptoService;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("api/v1/crypto")
-@Slf4j
+@RequestMapping("/api/v1/crypto")
+@RequiredArgsConstructor
+@Validated
 public class CryptoController {
-    @Autowired
-    private CryptoService cryptoService;
+    private final CryptoService cryptoService;
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public String home() {
-        log.info("Received request to GET /.");
         return String.format(
                 "Crypto Info API%n%n" +
-                        "Welcome to the crypto endpoint, you can make the following requests:%n" +
-                        "- GET /get-price%n" +
-                        "- GET /get-ohlc%n");
+                        "Welcome to the /crypto endpoint, you can make the following requests:%n" +
+                        "- GET /get-crypto%n" +
+                        "- GET /get-crypto-ts%n");
     }
 
-    @GetMapping(value = "/get-price", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<Crypto>> getPrice(@RequestParam String name) {
-        log.info("Received request to GET /get-price with argument: " + name.trim());
-        return cryptoService.getPrice(name.trim()).map(ResponseEntity::ok)
-        .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    @GetMapping(value = "/get-crypto", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Mono<GetCryptoResponse>> getCrypto(@Valid @RequestBody GetCryptoRequest getCryptoRequest) {
+        return ResponseEntity.ok(cryptoService.getCrypto(getCryptoRequest));
     }
 
-    @GetMapping(value = "/get-ohlc", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<CryptoTs>> getOhlc(@RequestParam String name, @RequestParam String days) {
-        log.info("Received request to GET /get-ohlc with arguments: " + name.trim() + ", " + days.trim());
-        return cryptoService.getOhlc(name.trim(), days.trim()).map(ResponseEntity::ok)
-        .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    @GetMapping(value = "/get-crypto-ts", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Mono<GetCryptoTsResponse>> getIntradayTs(
+            @Valid @RequestBody GetCryptoTsRequest getCryptoTsRequest) {
+        return ResponseEntity.ok(cryptoService.getCryptoTs(getCryptoTsRequest));
+    }
+
+    @GetMapping(value = "/*", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> invalid() {
+        return ResponseEntity
+                .ok("Invalid request, please refer to the README at https://github.com/sahilm8/crypto_info_svc");
     }
 }
