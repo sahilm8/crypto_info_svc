f4ea1e1e0903276c38a4d49eeed2edb06c3c8861
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-20 18:27:35 +0000

    Format CryptoController

--- src/main/java/com/sahil/crypto/info/controller/CryptoController.java
+++ src/main/java/com/sahil/crypto/info/controller/CryptoController.java
@@ -1,27 +1,21 @@
 package com.sahil.crypto.info.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sahil.crypto.info.dto.getCrypto.GetCryptoRequest;
 import com.sahil.crypto.info.dto.getCrypto.GetCryptoResponse;
 import com.sahil.crypto.info.dto.getCryptoTs.GetCryptoTsRequest;
 import com.sahil.crypto.info.dto.getCryptoTs.GetCryptoTsResponse;
-import com.sahil.crypto.info.model.Crypto;
-import com.sahil.crypto.info.model.CryptoTs;
 import com.sahil.crypto.info.service.CryptoService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @RestController
